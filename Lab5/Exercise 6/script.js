53034dc19a1ba316af27485fd54f2d47288c646f
Baragor2 <evgen3email@gmail.com>
2023-11-06 10:14:17 +0300

    ex6 ex7

---
+++ Lab5/Exercise 6/script.js
@@ -0,0 +1,19 @@
+function square(num) {
+    return num * num;
+}
+
+
+function sum_nums(num1, num2) {
+    return num1 + num2;
+}
+
+
+function dif_and_div(num1, num2, num3) {
+    return (num1 - num2) / num3;
+}
+
+
+function day_week(num) {
+    week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+    return week[num - 1];
+}
---
+++ Lab5/Exercise 7/script.js
@@ -0,0 +1,13 @@
+function is_equal(num1, num2) {
+    return num1 == num2 ? true : false;
+}
+
+
+function is_ten(num1, num2) {
+    return num1 + num2 > 10 ? true : false;
+}
+
+
+function is_negative(num) {
+    return num < 0 ? true : false;
+}
