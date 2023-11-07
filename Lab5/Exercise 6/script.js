b89cd1bae2813aa1e19f0443e29fb29befef1098
Baragor2 <evgen3email@gmail.com>
2023-11-07 09:58:16 +0300

    ex9 + changes

--- Lab5/Exercise 6/script.js
+++ Lab5/Exercise 6/script.js
@@ -4,7 +4,7 @@ function calc_square(num) {
 
 
 function calc_sum(num1, num2) {
-    return num1 + num2;
+    return Number(num1) + Number(num2);
 }
 
 
@@ -13,7 +13,23 @@ function calc_difDiv(num1, num2, num3) {
 }
 
 
-function get_dayWeek(num) {
+function get_dayWeek(day) {
     week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-    return week[num - 1];
+    return week[day - 1];
 }
+
+
+let cl_sq = prompt('Введите число');
+alert(calc_square(cl_sq));
+
+let cl_sum1 = prompt('Введите число');
+let cl_sum2 = prompt('Введите число');
+alert(calc_sum(cl_sum1, cl_sum2));
+
+let num1 = prompt('Введите число');
+let num2 = prompt('Введите число');
+let num3 = prompt('Введите число');
+alert(calc_difDiv(num1, num2, num3));
+
+let day = prompt('Введите число от 1 до 7');
+alert(get_dayWeek(day));
--- Lab5/Exercise 7/script.js
+++ Lab5/Exercise 7/script.js
@@ -4,10 +4,22 @@ function check_equal(num1, num2) {
 
 
 function check_ten(num1, num2) {
-    return num1 + num2 > 10 ? true : false;
+    return Number(num1) + Number(num2) > 10 ? true : false;
 }
 
 
 function check_negative(num) {
     return num < 0 ? true : false;
 }
+
+
+let ch_eq1 = prompt('Введите число');
+let ch_eq2 = prompt('Введите число');
+alert(check_equal(ch_eq1, ch_eq2));
+
+let num1 = prompt('Введите число');
+let num2 = prompt('Введите число');
+alert(check_ten(num1, num2));
+
+let num = prompt('Введите число');
+alert(check_negative(num));
